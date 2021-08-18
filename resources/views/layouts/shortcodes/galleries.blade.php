db1a3cf9c1f61c96678795a9367f8b5bd81a3b8c
Sanek <sanek@localhost.localdomain>
2021-08-18 17:20:43 +0300

    fix image in galleries template

--- resources/views/layouts/shortcodes/galleries.blade.php
+++ resources/views/layouts/shortcodes/galleries.blade.php
@@ -1,11 +1,11 @@
 <div class="row list__items" itemscope="" itemtype="http://schema.org/BlogPosting" itemprop="BlogPost">
     @foreach ($galleries as $gallery)
         <div class="col-md-6 col-sm-12 col-xs-12 center991">
-            @if(count($gallery->image))
+            @if($gallery->image)
             <div itemscope="" itemprop="image" itemtype="http://schema.org/ImageObject" class="img">
                 <figure>
                     <a href="{{ $gallery->urlGallery }}">
-                        <img itemprop="url contentUrl" src="{{ asset($gallery->image->getPath()) }}" class="img-add" alt="{{ $gallery->image->alt }}" title="{{ $gallery->image->title }}">
+                        <img itemprop="url contentUrl" src="{{ asset($gallery->image->path) }}" class="img-add" alt="{{ $gallery->image->alt }}" title="{{ $gallery->image->title }}">
                         <meta itemprop="url" content="{{ asset($gallery->image->path) }}">
                         <meta itemprop="width" content="365">
                         <meta itemprop="height" content="330">
